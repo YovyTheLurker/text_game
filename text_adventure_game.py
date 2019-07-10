b8aaaf64ae7034ae11da15f8f9e1baedbfb9c006
Yoevelyn Rodriguez <yoerodri@yahoo.com>
2019-07-10 01:41:45 -0400

    Code
    Adding the first lines of code and pseudocode.

---
+++ text_adventure_game.py
@@ -0,0 +1,29 @@
+# This is a text adventure game in the style of Zok.
+
+# In this game, I need to have text that moves the story along,
+# prompts to engage the player and a flow of the story based on
+# the answers chosen by the player.
+
+
+print("Hello, seeker.  Are you ready for your next adventure? Please, enter 'yes' to continue or 'no' to end the game.")
+
+print(str(input()))
+
+if str(input()) == "yes":
+    print("Great, grab you pointy hat and your magic wand because things are about to get fantastical for you!")
+else:
+    print("Very well...  I guess you don't have what it takes.")
+
+
+story_line1 = ['this is story one', 'this is story 2', 'this is story 3', 'this is story 4']
+def first_story():
+    for story in first_story(pass):
+        if one:
+            print("this")
+        elif two:
+            print("this other")
+        elif three:
+            print("yet another one")
+        else:
+            print("this one")
+
